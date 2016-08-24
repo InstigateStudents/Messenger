f81a2dc7b513c7762765f3fc7d9c37f1336f718d
Artur <arturgrigoryan0000@gmail.com>
2016-08-24 11:41:00 +0400

    Header file of the main core class

---
+++ core/dispatcher.hpp
@@ -0,0 +1,12 @@
+#ifndef DISPATCHER_HPP
+#define DISPATCHER_HPP
+
+/**
+ * @brief Dispatcher
+ *
+ * @details Provides API to the GUI for main messenger functionality.
+ */
+class Dispatcher {
+};
+
+#endif // DISPATCHER_HPP
